77f4d401cb3fcdb6c24875811f7494d1d715d75a
Brussels Sprout <34946565+brussels-sprout@users.noreply.github.com>
2020-07-20 22:02:10 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # Note: If you host the bot on Heroku (https://heroku.com/), you
 # need to assign your Discord bot token to a Config Var named "TOKEN".
 
-from setup import *
-from commands import *
+import setup
+import commands
