3c5f6c35d2a720d0a044fb2a3a41e1317bc730ec
Brussels Sprout <34946565+brussels-sprout@users.noreply.github.com>
2021-05-13 21:33:33 +0300

    Update tasks.py
    Remove ".invites" reminder

--- tasks.py
+++ tasks.py
@@ -22,11 +22,11 @@ async def set_nickname():
             pass
 
 
-@tasks.loop(hours=72)
-async def plasma_bot_maintenance():
-    await bot.get_channel(734117420888883231).send(
-        "<@&793981258291740723> Do `.invites`!"
-    )
+# @tasks.loop(hours=72)
+# async def plasma_bot_maintenance():
+#     await bot.get_channel(734117420888883231).send(
+#         "<@&793981258291740723> Do `.invites`!"
+#     )
 
 
 @tasks.loop(minutes=5)
